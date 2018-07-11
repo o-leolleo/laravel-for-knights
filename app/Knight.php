e8a589fea886142d4a5a5be466b6f82b2426f839
leolleocomp <leolleo.comp@gmail.com>
2018-07-10 21:49:45 -0300

    Add - relacionamentos cavaleiro-cavaleiro

--- app/Knight.php
+++ app/Knight.php
@@ -7,6 +7,14 @@ use Moloquent;
 class Knight extends Moloquent
 {
 	public function type() {
-		return $this->belongsTo('App\KnightType', 'knight_type_id', '_id');
+		return $this->belongsTo('App\KnightType', 'knight_type_id');
+	}
+
+	public function god() {
+		return $this->belongsTo('App\Knight', 'god_id');
+	}
+
+	public function knights() {
+		return $this->hasMany('App\Knight', 'god_id');
 	}
 }
