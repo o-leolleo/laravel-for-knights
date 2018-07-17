4773b707981300c233e8f238e650dfb6b9aa02bd
leolleocomp <leolleo.comp@gmail.com>
2018-07-17 20:22:14 -0300

    Add - CDZ logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+					<img src="./logo.png"/>
+                </div> 
             </div>
         </div>
     </body>
